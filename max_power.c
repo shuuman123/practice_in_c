175aa40cd4e11077c99531c191c18edefd2603d9
shuuman123 <152777223+shuuman123@users.noreply.github.com>
2023-12-04 05:01:14 +0800

    max_power.c

---
+++ max_power.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+int main(int argc, char *argv[])
+{
+  int a=0;
+  int b=0;
+  int pw=0;
+     printf ("Enter two integers: ");
+     scanf ("%d" "%d", &a, &b);
+        while(pow(a, pw) <  b){
+                pw++;
+        }//end while
+        if (pow(a, pw)  > b) {
+                pw--;
+        } // end if
+        printf ("%d\n", (int) pow(a, pw));
+}
